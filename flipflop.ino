eb4890dca044b01c716ca79069c141e6f0e8109f
Rayhan Emillul <rayhanemillul120@gmail.com>
2021-02-13 21:54:31 +0700

    Nomor 4

---
+++ flipflop.ino
@@ -0,0 +1,12 @@
+void setup(){
+    pinMode(7, OUTPUT); 
+    pinMode(8, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    delay(1000);
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+}
